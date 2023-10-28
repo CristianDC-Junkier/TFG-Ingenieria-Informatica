b694d5dedc5992bdc8eaa29d5070ae572952cf89
Cristian Delgado Cruz <Cristian@DESKTOP-1P2A66L>
2023-10-28 19:02:33 +0200

    Terminado diseño de menu vertical, arreglao bug de pantallas grandes con el menú introducción, falta hacer el diseño de VD, para terminar la parte de explorar

--- public_html/js/principalJS.js
+++ public_html/js/principalJS.js
@@ -1,7 +1,20 @@
 
-function dondeEstas()
+function dondeEstasHorizontal()
 {
-    const Titulos = document.querySelectorAll('.BarraHorizontalContenido');  
+    const Titulos = document.querySelectorAll('.BarraHorizontalContenido');
+
+    // Resaltar el enlace correspondiente a la página actual
+    Titulos.forEach(Titulos => {
+        if (window.location.href.includes(Titulos.getAttribute('href'))) {
+            Titulos.classList.add('active');
+        } else if (window.location.href.includes('introduccion') && Titulos.getAttribute('href').includes('inicio')) {
+            Titulos.classList.add('active');
+        }
+    });
+}
+function dondeEstasVertical()
+{
+    const Titulos = document.querySelectorAll('.barraVerticalContenido');
 
     // Resaltar el enlace correspondiente a la página actual
     Titulos.forEach(Titulos => {
@@ -13,7 +26,23 @@ function dondeEstas()
     });
 }
 
+function abrirMenuVertical() {
+    var botonMenuVertical = document.querySelector('.botonMenuVertical');
+    var barraVertical = document.querySelector('.barraVertical');
+
+    botonMenuVertical.addEventListener('click', function () {
+
+        if (barraVertical.style.display === 'none' || barraVertical.style.display === '') {
+            barraVertical.style.display = 'block';
+        } else {
+            barraVertical.style.display = 'none';
+        }
+    });
+}
+
+//Eventos
+document.addEventListener('DOMContentLoaded', abrirMenuVertical);
 
 //Ejecución
-dondeEstas();
-
+dondeEstasHorizontal();
+dondeEstasVertical();
