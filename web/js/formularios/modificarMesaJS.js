17f12553d1b705a2adf1681f33e4e4ca72d40a4c
Cristian <Cristian@DESKTOP-1P2A66L>
2023-12-31 02:10:26 +0100

    AJAX creado para todos las mesas, falta personajes(que lo voy a dejar para despues) y formularios. Además quiero reparar "explorar", añadir el cambio de foto de la mesa, y replicar nuevo estilo de color a todo, por último añadir el Htpps.

--- web/js/formularios/modificarMesaJS.js
+++ web/js/formularios/modificarMesaJS.js
@@ -1,23 +1,10 @@
-/*Colocar la fecha*/
-fechaNacimiento.setDate(fechaNacimiento.getDate() + 1);
-let formattedDate = fechaNacimiento.toISOString().split('T')[0];
-document.getElementById("bornReg").value = formattedDate;
-
-/*Colocar la provincia*/
-var provincias = document.getElementById("provinceReg");
-for (let i = 0; i < provincias.options.length; i++) {
-    if (provincias.options[i].value === provinciaActual) {
-        provincias.options[i].selected = true;
+/*Colocar la comunidad*/
+var comunidad = document.getElementById("comunityMesa");
+for (let i = 0; i < comunidad.options.length; i++) {
+    if (comunidad.options[i].value === comunidadActual) {
+        comunidad.options[i].selected = true;
         break;
     }
 }
 
-/*Colocar el genero*/
-var generos = document.getElementById("genderReg");
-for (let i = 0; i < generos.options.length; i++) {
-    if (generos.options[i].value === generoActual) {
-        generos.options[i].selected = true;
-        break;
-    }
-}
 
